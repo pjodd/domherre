5158a58b1b4800a9a7abcd54b395a7d23ed0de38
kalle <kalle@kodapan.se>
2017-03-22 21:47:01 +0100

    startup

--- src/main/java/se/kodapan/service/template/Main.java
+++ src/main/java/se/kodapan/service/template/Main.java
@@ -1,11 +1,34 @@
 package se.kodapan.service.template;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author kalle
  * @since 2017-03-22
  */
 public class Main {
 
+  public static void main(String[] args) throws Exception {
 
+    Logger log = LoggerFactory.getLogger(Main.class);
+
+    // todo insert your service implementation class here
+    Service service = new Service();
+
+    if (service.open()) {
+      Runtime.getRuntime().addShutdownHook(new Thread() {
+        public void run() {
+          try {
+            service.close();
+          } catch (Exception e) {
+            log.error("Exception while closing service", e);
+          }
+        }
+      });
+
+    }
+
+  }
 
 }
