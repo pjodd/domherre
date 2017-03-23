0a1d9bc9ac2bb012a09f0eac95583c8fafd12f76
kalle <kalle@kodapan.se>
2017-03-23 13:03:10 +0100

    reflection to find service implementation

--- src/main/java/se/kodapan/service/template/Main.java
+++ src/main/java/se/kodapan/service/template/Main.java
@@ -1,7 +1,14 @@
 package se.kodapan.service.template;
 
+import com.google.common.reflect.ClassPath;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import se.kodapan.service.template.prevalence.Transaction;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 /**
  * @author kalle
@@ -9,26 +16,64 @@ import org.slf4j.LoggerFactory;
  */
 public class Main {
 
+
   public static void main(String[] args) throws Exception {
 
     Logger log = LoggerFactory.getLogger(Main.class);
 
-    // todo insert your service implementation class here
-    Service service = new Service();
 
-    if (service.open()) {
-      Runtime.getRuntime().addShutdownHook(new Thread() {
-        public void run() {
-          try {
-            service.close();
-          } catch (Exception e) {
-            log.error("Exception while closing service", e);
+    List<Class<? extends Service>> serviceClasses = new ArrayList<>();
+
+    ClassLoader cl = Main.class.getClassLoader();
+    Set<ClassPath.ClassInfo> classesInPackage;
+
+    try {
+      classesInPackage = ClassPath.from(cl).getTopLevelClassesRecursive("se.kodapan.service");
+    } catch (IOException ioe) {
+      log.error("Exception caught during search for Service implementation class", ioe);
+      throw new RuntimeException(ioe);
+    }
+    for (ClassPath.ClassInfo classInfo : classesInPackage) {
+      Class foundClass;
+      try {
+        foundClass = classInfo.load();
+      } catch (NoClassDefFoundError e) {
+        continue;
+      }
+      if (!foundClass.equals(Service.class) && Service.class.isAssignableFrom(foundClass)) {
+        log.info("Found service class " + foundClass.getName());
+        serviceClasses.add(foundClass);
+      }
+
+    }
+
+    if (serviceClasses.isEmpty()) {
+      log.error("Unable to find any implementation of Service in classpath. Unable to start.");
+
+    } else if (serviceClasses.size() > 1) {
+      log.error("Multiple implementations of Service found in classpath. Unable to start.");
+
+    } else {
+
+      Service service = serviceClasses.get(0).newInstance();
+
+      if (service.open()) {
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+          public void run() {
+            try {
+              service.close();
+            } catch (Exception e) {
+              log.error("Exception while closing service", e);
+            }
           }
-        }
-      });
+        });
+
+      }
+
 
     }
 
   }
 
 }
+
