f80f49fffa87665b55035530d4a81ba690b2a454
kalle <kalle@kodapan.se>
2017-08-30 14:01:33 +0200

    Kafka as default

--- server/src/main/java/se/kodapan/service/template/mq/MessageQueueFactory.java
+++ server/src/main/java/se/kodapan/service/template/mq/MessageQueueFactory.java
@@ -1,9 +1,12 @@
 package se.kodapan.service.template.mq;
 
+import com.google.inject.ImplementedBy;
+
 /**
  * @author kalle
  * @since 2017-03-07 21:05
  */
+@ImplementedBy(se.kodapan.service.template.mq.kafka.KafkaFactory.class)
 public interface MessageQueueFactory {
 
   public abstract MessageQueueReader readerFactory(MessageQueueReaderConfiguration configuration, MessageQueueConsumer consumer);
